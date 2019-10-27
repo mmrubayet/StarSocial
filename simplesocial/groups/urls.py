cc2c580e2ef7d3e401e05c8565b0ba4efe0941c6
Rubayet Mahmud <m.rubayet94@gmail.com>
2019-10-27 10:05:07 +0600

    Update urls.py

--- simplesocial/groups/urls.py
+++ simplesocial/groups/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url('', views.ListGroups.as_view(), name='all'),
     url('new/' views.CreateGroup.as_view(), name='create'),
     url('posts/in/(?P<slug>[-\w]+)/', views.SingleGroup.as_view(), name='single')
-    
+    url('join/(?P<slug>[-\w]+)/', views.JoinGroup.as_view(),name='join')
+    url('leave/(?P<slug>[-\w]+)/', views.LeaveGroup.as_view(),name='leave')
+
 ]

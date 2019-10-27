ee316f019ef8d6c0501b8cf6ce8857e18de234db
Rubayet Mahmud <m.rubayet94@gmail.com>
2019-10-27 11:35:09 +0600

    Update urls.py

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace = 'accounts')),
     path('accounts/',include('django.contrib.auth.urls')),
+    path('posts/', include('posts.urls', namespace='posts')),
+    path('groups/', include('groups.urls', namespace='groups')),
 
 ]

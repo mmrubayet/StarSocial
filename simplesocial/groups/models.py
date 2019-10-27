494cdd311a5496215432d678f9a64819118e3935
Rubayet Mahmud <m.rubayet94@gmail.com>
2019-10-27 10:29:38 +0600

    update

--- simplesocial/groups/models.py
+++ simplesocial/groups/models.py
@@ -1,15 +1,20 @@
+from django.conf import settings
+from django.urls import reverse
 from django.db import models
 from django.utils.text import slugify
 
 #GROUPS MODELS.PY
-# Create your models here.
 import misaka
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
+
 from django import template
 register = template.Library()
+
+
 
 class Group(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -17,7 +22,6 @@ class Group(models.Model):
     description = models.TextField(blank=True, default='')
     description_html = models.TextField(editable=False, default='',blank=True)
     members = models.ManyToManyField(User, through='GroupMember')
-
 
     def __str__(self):
         return self.name
@@ -30,12 +34,14 @@ class Group(models.Model):
     def get_absolute_url(self):
         return reverse('groups:single', kwargs={'slug':self.slug})
 
+
     class Meta:
         ordering = ['name']
 
+
 class GroupMember(models.Model):
-    group = models.ForeignKey(Group, related_name='membership', on_delete=models.CASCADE)
-    user = models.ForeignKey(Group, related_name='user_groups', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, related_name='memberships', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_groups', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username

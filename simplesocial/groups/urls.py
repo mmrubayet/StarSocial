cc02ebbc4413a966bdc79675cf51bf17f812304e
Rubayet Mahmud <m.rubayet94@gmail.com>
2019-10-27 11:35:41 +0600

    Update urls.py

--- simplesocial/groups/urls.py
+++ simplesocial/groups/urls.py
@@ -1,14 +1,14 @@
 # GROUPS URLS.PY
 from django.conf.urls import url
-from . import views as
+from . import views
 
 app_name = 'groups'
 
 urlpatterns = [
-    url('', views.ListGroups.as_view(), name='all'),
-    url('new/' views.CreateGroup.as_view(), name='create'),
-    url('posts/in/(?P<slug>[-\w]+)/', views.SingleGroup.as_view(), name='single')
-    url('join/(?P<slug>[-\w]+)/', views.JoinGroup.as_view(),name='join')
-    url('leave/(?P<slug>[-\w]+)/', views.LeaveGroup.as_view(),name='leave')
+    url('^$', views.ListGroups.as_view(), name='all'),
+    url('new/', views.CreateGroup.as_view(), name='create'),
+    url('posts/in/(?P<slug>[-\w]+)/', views.SingleGroup.as_view(), name='single'),
+    url('join/(?P<slug>[-\w]+)/', views.JoinGroup.as_view(),name='join'),
+    url('leave/(?P<slug>[-\w]+)/', views.LeaveGroup.as_view(),name='leave'),
 
 ]
